426c9fbd3d576793d4e56f5a40371b16995872f5
RainingHeart <lzy_2318@163.com>
2019-05-29 21:38:38 +0800

    搭建基础结构与展示文章数据

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+from .models import Post, Tag
 # Create your views here.
+
+
+def post_list(request, category_id=None, tag_id=None):
+    if tag_id:
+        try:
+            tag = Tag.objects.get(id=tag_id)
+        except Tag.DoesNotExist:
+            post_li = []
+        else:
+            post_li = tag.post_set.filter(status=Post.STATUS_NORMAL)
+    else:
+        post_li = Post.objects.filter(status=Post.STATUS_NORMAL)
+        if category_id:
+            post_li = post_li.filter(category_id=category_id)
+
+    return render(request, 'blog/list.html', context={'post_list': post_li})
+
+
+def post_detail(request, post_id):
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        post = None
+    return render(request, 'blog/detail.html', context={'post': post})
