a083b17582ea69c7addeb5d9e5d512b1faec27cc
RainingHeart <lzy_2318@163.com>
2019-06-02 22:39:52 +0800

    配置页面通用数据，封装侧边栏逻辑，整理模板代码

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,24 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Post, Tag
+from config.models import SideBar
+from .models import Post, Tag, Category
 # Create your views here.
 
 
 def post_list(request, category_id=None, tag_id=None):
-    if tag_id:
-        try:
-            tag = Tag.objects.get(id=tag_id)
-        except Tag.DoesNotExist:
-            post_li = []
-        else:
-            post_li = tag.post_set.filter(status=Post.STATUS_NORMAL)
-    else:
-        post_li = Post.objects.filter(status=Post.STATUS_NORMAL)
-        if category_id:
-            post_li = post_li.filter(category_id=category_id)
+    tag = None
+    category = None
 
-    return render(request, 'blog/list.html', context={'post_list': post_li})
+    if tag_id:
+        post_li, tag = Post.get_by_tag(tag_id)
+    elif category_id:
+        post_li, category = Post.get_by_category(category_id)
+    else:
+        post_li = Post.latest_posts()
+
+    context = {
+        'category': category,
+        'tag': tag,
+        'post_list': post_li,
+        'sidebars': SideBar.get_all(),
+    }
+    context.update(Category.get_navs())
+    return render(request, 'blog/list.html', context=context)
 
 
 def post_detail(request, post_id):
@@ -26,4 +32,10 @@ def post_detail(request, post_id):
         post = Post.objects.get(id=post_id)
     except Post.DoesNotExist:
         post = None
-    return render(request, 'blog/detail.html', context={'post': post})
+
+    context = {
+        'post': post,
+        'sidebars': SideBar.get_all(),
+    }
+    context.update(Category.get_navs())
+    return render(request, 'blog/detail.html', context=context)
